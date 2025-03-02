fcb2d41cfb27ed2568a978ba5e8a2041317e1ca9
anxinyu <anxinyuwork@163.com>
2025-03-02 11:54:54 +0800

    func: update msql conf

--- src/main/resources/sql/create_table.sql
+++ src/main/resources/sql/create_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE book (
-                       id BIGINT NOT NULL AUTO_INCREMENT COMMENT '图书ID',
-                       title VARCHAR(255) NOT NULL COMMENT '标题',
-                       author VARCHAR(255) NOT NULL COMMENT '作者',
-                       publicationYear INT NOT NULL COMMENT '出版年份',
-                       price DOUBLE NOT NULL COMMENT '价格',
-                       createdTime DATETIME NOT NULL COMMENT '创建时间',
-                       updatedTime DATETIME NOT NULL COMMENT '更新时间',
-                       PRIMARY KEY (id)
+    id BIGINT NOT NULL AUTO_INCREMENT COMMENT '图书ID',
+    title VARCHAR(255) NOT NULL COMMENT '标题',
+    author VARCHAR(255) NOT NULL COMMENT '作者',
+    publicationYear INT NOT NULL COMMENT '出版年份',
+    price DOUBLE NOT NULL COMMENT '价格',
+    createdTime DATETIME NOT NULL COMMENT '创建时间',
+    updatedTime DATETIME NOT NULL COMMENT '更新时间',
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB;
